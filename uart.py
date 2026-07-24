142844302d0c4b3a01642986f09845ab3fd602ad
Phu <lepq@mail.uc.edu>
2026-07-24 11:12:47 -0400

    update pyserial

--- uart.py
+++ uart.py
@@ -5,25 +5,24 @@ import time
 ser = serial.Serial('/dev/ttyUSB3', 115200) 
 print(f"Connected to: {ser.name}")
 
+#reset buffer
 ser.reset_input_buffer()
 ser.reset_output_buffer()
 
-message = b'Hello FPGA!\n'
-print(f"Sending: {message}")
+message = input("Type message: ").encode(encoding="utf-8",errors="replace")
+print(f"Sending: {message.decode(encoding="utf-8",errors="replace")}")
 
-# FIX: Send character-by-character with a tiny delay
 for byte in message:
     ser.write(bytes([byte]))
-    time.sleep(0.002) 
-
-time.sleep(0.1)
+    
+time.sleep(0.05)
 
 bytes_waiting = ser.in_waiting
 if bytes_waiting > 0:
     response = ser.read(bytes_waiting)
-    print(f"Received back cleanly: {response}")
+    decoded_response= response.decode(encoding="utf-8",errors="replace")
+    print(f"Received: {decoded_response}")
 else:
-    print("No data received back.")
+    print("No data")
 
 ser.close()
-
